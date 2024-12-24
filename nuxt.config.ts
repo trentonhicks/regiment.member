88d0e85851743a528d125ef86ad9f8498cff2deb
Trenton Hicks <trentonmhicks@gmail.com>
2024-12-24 15:52:57 -0600

    Change base url for production.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,13 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
   ssr: false,
+  app: {
+    baseURL: process.env.DEPLOY_ENV === 'GH_PAGES' ? '/regiment.member/' : '/'
+  },
   router: {
     options: {
       hashMode: true,
-    }
+    },
   },
   css: ['~/assets/css/main.css'],
   postcss: {
